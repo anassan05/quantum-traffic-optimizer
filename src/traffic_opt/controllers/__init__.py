cdae105739f5ce0c71f8b751f4391fce53bec42e
anassan05 <mohammedanass2005.com>
2026-09-19 15:28:56 +0530

    Integrate quantum hybrid traffic controller

--- src/traffic_opt/controllers/__init__.py
+++ src/traffic_opt/controllers/__init__.py
@@ -3,10 +3,13 @@
 from .base import SignalController, SignalDecision
 from .adaptive import AdaptiveController
 from .fixed_time import FixedTimeController
+from .quantum_hybrid import QuantumHybridComparison, QuantumHybridController
 
 __all__ = [
 	"AdaptiveController",
 	"FixedTimeController",
+	"QuantumHybridComparison",
+	"QuantumHybridController",
 	"SignalController",
 	"SignalDecision",
 ]
